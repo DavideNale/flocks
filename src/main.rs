2d9fb2f557f51b73839271ae8f9fa55ac89d2d3e
Davide Nale <davide.nale.99@gmail.com>
2024-02-29 22:37:10 +0100

    Added boid definition and grid generation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,58 @@
 use nannou::prelude::*;
 
-struct Model {}
-
 fn main() {
-    nannou::app(model).event(event).simple_window(view).run();
+    nannou::app(model).update(update).run();
 }
 
-fn model(_app: &App) -> Model {
-    Model {}
+struct Boid {
+    x: f32,
+    y: f32,
 }
 
-fn event(_app: &App, _model: &mut Model, _event: Event) {}
+struct Model {
+    boids: Vec<Boid>,
+    num_boids: usize,
+}
 
-fn view(_app: &App, _model: &Model, _frame: Frame) {}
+fn model(app: &App) -> Model {
+    app.new_window()
+        .size(1000, 1000)
+        .view(view)
+        .build()
+        .unwrap();
+
+    let num_boids = 1000;
+    let boids = generate_boids_grid(num_boids, app.window_rect());
+
+    Model { boids, num_boids }
+}
+
+fn generate_boids_grid(num_boids: usize, _rect: Rect) -> Vec<Boid> {
+    // let w = rect.w();
+    // let h = rect.h();
+
+    let side = (num_boids as f32).sqrt().ceil() as usize;
+    let delta: f32 = 10.0;
+    let boids: Vec<Boid> = (0..side)
+        .flat_map(|i| {
+            (0..side).map(move |j| Boid {
+                x: i as f32 * delta - (side as f32 * delta) / 2.0,
+                y: j as f32 * delta - (side as f32 * delta) / 2.0,
+            })
+        })
+        .collect();
+    boids
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(WHITE);
+
+    model.boids.iter().for_each(|b| {
+        draw.ellipse().x_y(b.x, b.y).radius(2.0).color(BLACK);
+    });
+
+    draw.to_frame(app, &frame).unwrap();
+}
