66fc831be1c5a730d100f67ae84590ecc9845add
Davide Nale <davide.nale.99@gmail.com>
2024-03-02 17:25:05 +0100

    Added classic boids implementation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use nannou::prelude::*;
+use std::f64::MANTISSA_DIGITS;
+
+use nannou::{prelude::*, text::cursor::closest_cursor_index_and_xy};
 
 fn main() {
     nannou::app(model).update(update).run();
@@ -7,28 +9,92 @@ fn main() {
 struct Boid {
     x: f32,
     y: f32,
+    vx: f32,
+    vy: f32,
 }
 
 fn update_boids(boids: &mut Vec<Boid>, dt: f32) {
+    let turn_factor: f32 = 0.2;
+    let visual_range: f32 = 100.0;
+    let protected_range: f32 = 15.0;
+    let centering_factor: f32 = 0.005;
+    let avoid_factor: f32 = 0.05;
+    let matching_factor: f32 = 0.05;
+    let speed_max: f32 = 6.0;
+    let speed_min: f32 = 3.0;
+
     for i in 0..boids.len() {
-        let protected: f32 = 50.0;
+        // Save i-th boid's transform
+        let x = boids[i].x;
+        let y = boids[i].y;
+        let mut vx = boids[i].vx;
+        let mut vy = boids[i].vy;
+
+        // Zero accumulators
         let mut close_dx: f32 = 0.0;
         let mut close_dy: f32 = 0.0;
 
-        let x = boids[i].x;
-        let y = boids[i].y;
+        let mut xvel_avg: f32 = 0.0;
+        let mut yvel_avg: f32 = 0.0;
+        let mut neighboring_boids: f32 = 0.0;
 
-        for other_boid in &mut *boids {
-            let distance2 = (other_boid.x - x).powi(2) + (other_boid.y - y).powi(2);
+        let mut xpos_avg: f32 = 0.0;
+        let mut ypos_avg: f32 = 0.0;
 
-            if distance2 != 0.0 && distance2 <= protected.powi(2) {
-                close_dx += other_boid.x;
-                close_dy += other_boid.y;
+        // Iterate each boid
+        for j in 0..boids.len() {
+            if i == j {
+                continue;
+            }
+
+            let other_x: f32 = boids[j].x;
+            let other_y: f32 = boids[j].y;
+            let other_vx: f32 = boids[j].vx;
+            let other_vy: f32 = boids[j].vy;
+
+            let dx = x - other_x;
+            let dy = y - other_y;
+            let dist = (dx * dx + dy * dy).sqrt();
+
+            if dist < visual_range {
+                // Separation
+                if dist < protected_range {
+                    close_dx += dx;
+                    close_dy += dy;
+                }
+
+                // Alignment
+                xvel_avg += other_vx;
+                yvel_avg += other_vy;
+
+                // Cohesion
+                xpos_avg += other_x;
+                ypos_avg += other_y;
+
+                neighboring_boids += 1.0;
             }
         }
+        // Update i-th boid's transform
 
-        boids[i].x -= close_dx * dt;
-        boids[i].y -= close_dy * dt;
+        if neighboring_boids > 0.0 {
+            xvel_avg = xvel_avg / neighboring_boids;
+            yvel_avg = yvel_avg / neighboring_boids;
+            xpos_avg = xpos_avg / neighboring_boids;
+            ypos_avg = ypos_avg / neighboring_boids;
+
+            vx += (xpos_avg - x) * centering_factor + (xvel_avg - vx) * matching_factor;
+            vy += (ypos_avg - y) * centering_factor + (yvel_avg - vy) * matching_factor;
+        }
+
+        vx += close_dx * avoid_factor;
+        vy += close_dy * avoid_factor;
+
+        // println!("{} {}", vx, vy);;
+        boids[i].vx = vx;
+        boids[i].vy = vy;
+
+        boids[i].x += vx * dt;
+        boids[i].y += vy * dt;
     }
 }
 
@@ -44,7 +110,7 @@ fn model(app: &App) -> Model {
         .build()
         .unwrap();
 
-    let num_boids = 1000;
+    let num_boids = 100;
     let boids = generate_boids_grid(num_boids, app.window_rect());
 
     Model { boids, num_boids }
@@ -61,13 +127,17 @@ fn generate_boids_grid(num_boids: usize, _rect: Rect) -> Vec<Boid> {
             (0..side).map(move |j| Boid {
                 x: i as f32 * delta - (side as f32 * delta) / 2.0,
                 y: j as f32 * delta - (side as f32 * delta) / 2.0,
+                vx: 0.0,
+                vy: 0.0,
             })
         })
         .collect();
+
     boids
 }
 
 fn update(_app: &App, model: &mut Model, update: Update) {
+    // println!("{}", update.since_last.as_secs_f32());
     update_boids(&mut model.boids, update.since_last.as_secs_f32());
 }
 
